7ad8d688f57d24bf402462fd5af567d15c237d73
Gamepr <71444980+Gamepr@users.noreply.github.com>
2020-10-10 23:54:10 +0200

    UI update
    UI heeft een uiterlijk updateje gehad en pause menu toegevoegd

---
+++ GameDesign2020_group13/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+
+
+public class PauseMenu : MonoBehaviour
+
+{
+
+    public static bool GameIsPaused = false;
+
+
+
+    public GameObject pauseMenuUI;
+
+
+
+    void Start()
+
+    {
+
+
+
+        pauseMenuUI.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        GameIsPaused = false;
+
+    }
+
+
+
+    void Update()
+
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+
+        {
+
+            if (GameIsPaused)
+
+            {
+
+                Resume();
+
+            }
+
+            else
+
+            {
+
+                Pause();
+
+            }
+
+        }
+
+    }
+
+
+
+    public void Resume()
+
+    {
+
+        pauseMenuUI.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        GameIsPaused = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+        Cursor.visible = false;
+
+    }
+
+    void Pause()
+
+    {
+
+        pauseMenuUI.SetActive(true);
+
+        Time.timeScale = 0f;
+
+        GameIsPaused = true;
+
+        Cursor.lockState = CursorLockMode.None;
+
+        Cursor.visible = true;
+
+    }
+
+
+
+    public void LoadMenu()
+
+
+
+    {
+
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene("UI");
+
+    }
+
+
+
+    public void QuitGame()
+
+    {
+
+        Debug.Log("Quitting game...");
+
+        Application.Quit();
+
+    }
+
+}
