14acb8f112bbfc57885890703e4926142c9c5fac
Gamepr <71444980+Gamepr@users.noreply.github.com>
2020-10-20 00:56:14 +0200

    UI
    Begin gemaakt aan biggetjes wisselen in UI

---
+++ GameDesign2020_group13/Assets/Scripts/TopUI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.SceneManager;
+
+public class TopUI : MonoBehaviour
+{
+    int pig = 0;
+    void ChangePig()
+    {
+        if (pig == 0)
+        {
+            Pig1();
+            pig++;
+        }
+        else if( pig == 1)
+        {
+            Pig2();
+            pig++;
+        }
+        else if (pig == 2)
+        {
+            Pig3();
+            pig++;
+        }
+        else
+        {
+            pig = 0;
+        }
+    }
+    void Pig1()
+    {
+        //Image Pig veranderen ofzo en de andere 2 niet
+    }
+    void Pig2()
+    {
+
+    }
+    void Pig3()
+    {
+
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ChangePig();
+        }
+    }
+}
