5b17abf2e2a255d14d8ac2b5dce2d141fb817742
tjheij <tjheij@gmail.com>
2020-10-19 13:48:48 +0200

    Wolf pathfinding
    When disguised and a wolf sees you, he follows you. When he doesn't see you anymore, he goes back to his patrol. Fixed finish in game level and switching players. Put wolf model in prefab

--- GameDesign2020_group13/Assets/Scripts/GameMaster.cs
+++ GameDesign2020_group13/Assets/Scripts/GameMaster.cs
@@ -20,6 +20,15 @@ public class GameMaster : MonoBehaviour
 	public Transform spawnpoint;
 	public int keys;
 
+	public GameObject[] players;
+	public int currentPlayer = 0;
+
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.Tab)) {
+			switchPlayer();
+		}
+	}
+
 	//Respawns the player at current spawnpoint
 	public void respawnPlayer(GameObject player) {
 		player.transform.position = spawnpoint.position;
@@ -52,6 +61,30 @@ public class GameMaster : MonoBehaviour
 			return true;
 		} else {
 			return false;
+		}
+	}
+
+	//Cycle through players by deactivating active player and activating next player
+	private void switchPlayer() {
+		//Switch off current player
+		players[currentPlayer].transform.Find("Camera").GetComponent<Camera>().enabled = false;
+		players[currentPlayer].GetComponent<PlayerMovement>().enabled = false;
+		players[currentPlayer].GetComponent<PlayerLookAround>().enabled = false;
+		if (players[currentPlayer].GetComponent<PlayerDisguise>() != null) {
+			players[currentPlayer].GetComponent<PlayerDisguise>().enabled = false;
+		}
+
+		currentPlayer++;
+		if (currentPlayer == players.Length) {
+			currentPlayer = 0;
+		}
+
+		//Switch on next player
+		players[currentPlayer].transform.Find("Camera").GetComponent<Camera>().enabled = true;
+		players[currentPlayer].GetComponent<PlayerMovement>().enabled = true;
+		players[currentPlayer].GetComponent<PlayerLookAround>().enabled = true;
+		if (players[currentPlayer].GetComponent<PlayerDisguise>() != null) {
+			players[currentPlayer].GetComponent<PlayerDisguise>().enabled = true;
 		}
 	}
 }
--- GameDesign2020_group13/Assets/Scripts/Wolf.cs
+++ GameDesign2020_group13/Assets/Scripts/Wolf.cs
@@ -1,15 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Wolf : MonoBehaviour
 {
 	private RaycastHit vision;
-	public int sightReach = 5;
 	public int fov = 60;
-	GameObject player;
-	Rigidbody rb;
-	public int speed = 2;
 
 	public Transform[] patrolPoints;
 	int currentPatrolPoint = 0;
@@ -17,37 +14,41 @@ public class Wolf : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		rb = GetComponent<Rigidbody>();
-		transform.LookAt(patrolPoints[0].position);
+		GetComponent<NavMeshAgent>().SetDestination(patrolPoints[currentPatrolPoint].position);
 	}
 
     // Update is called once per frame
     void Update()
     {
-		if(player == null) {
-			player = GameObject.FindGameObjectWithTag("Player");
-		}
+		Vector2 transformVector = new Vector2(transform.position.x, transform.position.z);
+		Vector2 patrolVector = new Vector2(patrolPoints[currentPatrolPoint].position.x, patrolPoints[currentPatrolPoint].position.z);
 
-		if (Vector3.Distance(transform.position, player.transform.position) < sightReach) {
-			float angle = Vector3.Angle(player.transform.position - transform.position, transform.forward);
-			if (Mathf.Abs(angle) <= fov) {
-				if(Physics.Raycast(transform.position, player.transform.position - transform.position, out vision)) {
-					if (vision.collider.CompareTag("Player")) {
-						GameMaster.Instance.respawnPlayer(vision.collider.gameObject);
-					}
-				}
-			}
-		}
-
-		if(Vector3.Distance(transform.position, patrolPoints[currentPatrolPoint].position) == 0) {
+		if(Vector2.Distance(transformVector, patrolVector) <= 0.5f) {
 			currentPatrolPoint++;
 			if(currentPatrolPoint >= patrolPoints.Length) {
 				currentPatrolPoint = 0;
 			}
-			transform.LookAt(patrolPoints[currentPatrolPoint].position);
+			GetComponent<NavMeshAgent>().SetDestination(patrolPoints[currentPatrolPoint].position);
 		}
-		transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPatrolPoint].position, speed * Time.deltaTime);
-		
 
+	}
+
+	private void OnTriggerStay(Collider other) {
+		if (other.CompareTag("Player")) {
+			float angle = Vector3.Angle(other.transform.position - transform.position, transform.forward);
+			if (Mathf.Abs(angle) <= 0.5*fov) {
+				if (Physics.Raycast(transform.position, other.transform.position - transform.position, out vision)) {
+					GameMaster.Instance.respawnPlayer(vision.collider.gameObject);
+				}
+			}
+		} else if(other.CompareTag("DisguisedPlayer")){
+			GetComponent<NavMeshAgent>().SetDestination(other.transform.position);
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.CompareTag("DisguisedPlayer")) {
+			GetComponent<NavMeshAgent>().SetDestination(patrolPoints[currentPatrolPoint].position);
+		}
 	}
 }
